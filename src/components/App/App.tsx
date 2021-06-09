2831605ffec422d09b33a56183f4ed3ee16fb9ca
Gilad Tsabar <gilad@tsabar.net>
2021-06-09 18:34:09 +0200

    fix to level selection buttons

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -25,9 +25,9 @@ const App:React.FC = ():JSX.Element => {
         <aside>
           <div>
             <h1>Current Level:</h1>
-            <button name="0" disabled={localStorage.length<1} onClick={switchLevel}>Easy</button>
-            <button name="1" disabled={localStorage.length<2} onClick={switchLevel}>Intermediate</button>
-            <button name="2" disabled={localStorage.length<3} onClick={switchLevel}>Hard</button>
+            <button name="0" disabled={localStorage.getItem('level-0-solution') === null} onClick={switchLevel}>Easy</button>
+            <button name="1" disabled={localStorage.getItem('level-1-solution') === null} onClick={switchLevel}>Intermediate</button>
+            <button name="2" disabled={localStorage.getItem('level-2-solution') === null} onClick={switchLevel}>Hard</button>
           </div>
           <button onClick={e=>{
             startGame(true);
@@ -37,7 +37,7 @@ const App:React.FC = ():JSX.Element => {
               Help:
             </h1>
             <ul>
-              <li>Use the mouse or TAB key to select the square you want to fill.</li>
+              <li>Use the mouse to select the square you want to fill.</li>
               <li>Type any number between 1 and 9. Delete with the backspace key.</li>
               <li>Once level is correctly completed, you will be able to start the next level.</li>
               <li>Have fun playing!</li>
